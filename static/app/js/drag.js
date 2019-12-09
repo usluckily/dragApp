f0eee3af2aa44509ce6c50125af7702b7f95f6e9
usluckily <763806142@qq.com>
2019-12-09 17:37:32 +0800

    增加拖动边缘碰撞

--- static/app/js/drag.js
+++ static/app/js/drag.js
@@ -5,13 +5,23 @@
 $(function() {
 	$(document).mousemove(function(e) {
 		if (!!this.move) {
-			var posix = !document.move_target ? {'x': 0, 'y': 0} : document.move_target.posix,
-				callback = document.call_down || function() {
-					$(this.move_target).css({
-						'top': e.pageY - posix.y,
-						'left': e.pageX - posix.x
-					});
-				};
+			var posix = !document.move_target ? {'x': 0, 'y': 0} : document.move_target.posix
+
+			var xNum = e.pageX - posix.x + parseFloat(this.itemW), 
+				yNum = e.pageY - posix.y + parseFloat(this.itemH)
+
+			xNum = xNum > this.boxWidth ? this.boxWidth - parseInt(this.itemW) : 
+				xNum <= parseInt(this.itemW) ? 0 : null
+				
+			yNum = yNum > this.boxHeight ? this.boxHeight - parseInt(this.itemH) : 
+				yNum <= parseInt(this.itemH) ? 0 : null
+
+			var callback = document.call_down || function() {
+				$(this.move_target).css({
+					'top': yNum === null ? (e.pageY - posix.y) : yNum,
+					'left': xNum === null ? (e.pageX - posix.x) : xNum
+				});
+			};
 
 			callback.call(this, e, posix);
 		}
@@ -28,25 +38,25 @@ $(function() {
 		}
 	});
 
-	var $box = $('#box').mousedown(function(e) {
-	    var offset = $(this).offset();
+	// var $box = $('#box').mousedown(function(e) {
+	//     var offset = $(this).offset();
 	    
-	    this.posix = {'x': e.pageX - offset.left, 'y': e.pageY - offset.top};
-	    $.extend(document, {'move': true, 'move_target': this});
-	}).on('mousedown', '#coor', function(e) {
-	    var posix = {
-	            'w': $box.width(), 
-	            'h': $box.height(), 
-	            'x': e.pageX, 
-	            'y': e.pageY
-	        };
+	//     this.posix = {'x': e.pageX - offset.left, 'y': e.pageY - offset.top};
+	//     $.extend(document, {'move': true, 'move_target': this});
+	// }).on('mousedown', '#coor', function(e) {
+	//     var posix = {
+	//             'w': $box.width(), 
+	//             'h': $box.height(), 
+	//             'x': e.pageX, 
+	//             'y': e.pageY
+	//         };
 	    
-	    $.extend(document, {'move': true, 'call_down': function(e) {
-	        $box.css({
-	            'width': Math.max(30, e.pageX - posix.x + posix.w),
-	            'height': Math.max(30, e.pageY - posix.y + posix.h)
-	        });
-	    }});
-	    return false;
-	});
+	//     $.extend(document, {'move': true, 'call_down': function(e) {
+	//         $box.css({
+	//             'width': Math.max(30, e.pageX - posix.x + posix.w),
+	//             'height': Math.max(30, e.pageY - posix.y + posix.h)
+	//         });
+	//     }});
+	//     return false;
+	// });
 });
